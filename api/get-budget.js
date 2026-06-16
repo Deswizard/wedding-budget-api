86e45a1957154d960c48ad009d4fb01605656127
Deswizard <32955081+Deswizard@users.noreply.github.com>
2026-06-16 18:52:04 +0300

    Update get-budget.js

--- api/get-budget.js
+++ api/get-budget.js
@@ -29,8 +29,8 @@ export default async function handler(req, res) {
       totals[category] = (totals[category] || 0) + value;
     });
 
-    res.status(200).json({ totals });
+    return res.status(200).json({ totals });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 }
