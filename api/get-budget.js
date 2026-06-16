04c5c180f11abd3770f40360f2415172a4d36399
Deswizard <32955081+Deswizard@users.noreply.github.com>
2026-06-16 15:50:47 +0300

    Update get-budget.js
    Replaced entire code. Previous code was not pulling categories and totals properly. HTML chart was breaking.

--- api/get-budget.js
+++ api/get-budget.js
@@ -1,9 +1,3 @@
-import { Client } from "@notionhq/client";
-
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-});
-
 export default async function handler(req, res) {
   try {
     const response = await notion.databases.query({
